cd8e2fe56d33f12323aae50313073054324bb0f4
Andy Armstrong <andy@hexten.net>
2014-08-07 21:26:50 +0100

    Config update.

--- config.sh
+++ config.sh
@@ -3,9 +3,10 @@
 export USER=root
 export DB=c0wshedyoga
 export OUT=sql
-export INDOM=centred.co.uk
+export INDOM=centered.co.uk
 #export OUTDOM=centred.tthtesting.co.uk
-export OUTDOM=centred.hexten.net
+export OUTDOM=centered.tthtesting.co.uk
+export FLAG='/tmp/centred-data.flag'
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
 
--- pullloop.sh
+++ pullloop.sh
@@ -4,18 +4,17 @@
 
 eval $( ./find-mysql.sh )
 
-flag='/tmp/rf-data.flag'
 while ssleep 10; do
   git pull
-  if [ -e "$flag" ]; then
-    prev=$( cat "$flag" )
+  if [ -e "$FLAG" ]; then
+    prev=$( cat "$FLAG" )
     head=$( cat .git/refs/heads/master )
     git diff --numstat $prev..$head | awk -e '{ print $3 }' | grep '^sql/' | while read tbl; do
       echo "Loading $tbl"
       cat "$tbl" | perl domhack.pl $INDOM $OUTDOM | $MYSQL -u$USER $DB
     done
   fi
-  cp .git/refs/heads/master "$flag"
+  cp .git/refs/heads/master "$FLAG"
 done
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
